b8c2299fbf7bbd70cdadf62d72174917cb69a819
Eduardo Picazo <eduardo@kanto.studio>
2021-03-29 23:04:58 -0600

    Converts to a pwa

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,11 +10,28 @@ function App({ Component, pageProps }: AppProps) {
       <Layout>
         <Head>
           <title>NextJS Events</title>
+          <meta charSet="utf-8" />
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
           <meta
             name="viewport"
-            content="initial-scale=1.0, width=device-width"
+            content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
           />
           <meta name="description" content="NextJS Events App" />
+          <link rel="manifest" href="/manifest.json" />
+          <link
+            href="/icons/favicon-16x16.png"
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+          />
+          <link
+            href="/icons/favicon-32x32.png"
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+          />
+          <link rel="apple-touch-icon" href="/apple-icon.png"></link>
+          <meta name="theme-color" content="#317EFB" />
         </Head>
         <Component {...pageProps} />
       </Layout>
