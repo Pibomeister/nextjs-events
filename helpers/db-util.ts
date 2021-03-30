cbfce3087c294b785e19c2f701886b90731ad654
Eduardo Picazo <eduardo@kanto.studio>
2021-03-29 22:37:48 -0600

    Removes env file

--- helpers/db-util.ts
+++ helpers/db-util.ts
@@ -2,7 +2,7 @@ import { MongoClient } from 'mongodb';
 
 export async function connectDatabase() {
   const client = await MongoClient.connect(
-    'mongodb+srv://admin:SaAxvlU02PcMLUUi@cluster0.e5tix.mongodb.net/events?retryWrites=true&w=majority',
+    process.env.MONGODB_URI,
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
